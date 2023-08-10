82a87e88ed0285999933de1961535a18db41d549
Ruan fonseca <whitelook22@outlook.com>
2023-08-10 16:43:58 -0300

    criando crud de estado

---
+++ backend/src/main/java/com/loja/backend/entities/Estado.java
@@ -0,0 +1,33 @@
+package com.loja.backend.entities;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name="estado")
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+public class Estado {
+
+     @Id
+     @GeneratedValue(strategy = GenerationType.AUTO) //automaticamente
+     private Long id;
+
+
+     private String nome;
+     private String sigla;
+
+     @Temporal(TemporalType.TIMESTAMP) //informando data e hora
+     private Date dataCriacao;
+
+    @Temporal(TemporalType.TIMESTAMP)
+     private Date dataAtualizacao;
+
+
+}
